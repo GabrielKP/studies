976e3db14621a641956722e7417337fbb7b0bf61
Gabriel Kressin Palacios <gkressi1@jhu.edu>
2024-02-09 22:32:58 -0500

    Transportation questionnaires

---
+++ www/study-q/static/js/stage/QuestionnaireTransportation.js
@@ -0,0 +1,65 @@
+define(["component/Pages"], function (Pages) {
+  let study;
+  let pages;
+  let warning_text =
+    '<div class="alert alert-danger"><i>Please answer all questions!</i></div>';
+
+  function _finish_task() {
+    $(".collectible").each(function () {
+      study.data.record_trialdata({
+        status: "ongoing",
+        question: this.id,
+        answer: this.value,
+      });
+    });
+    pages.next();
+  }
+
+  return {
+    name: "questionnaire_transportation",
+    init: function (_study) {
+      study = _study;
+      pages = new Pages();
+      return pages.init(
+        study,
+        ["questionnaires/transportation.html"],
+        function () {
+          study.next();
+        }
+      );
+    },
+    show: function () {
+      pages.next();
+
+      // bind conditional enable
+      $(".collectible").on("change", function () {
+        // add changed class for tracking
+        $(this).addClass("changed");
+
+        // check for all changed
+        if ($("select.changed").length === $("select.collectible").length) {
+          $("#warning").html("");
+          $("#submit")
+            .off()
+            .on("click", function () {
+              _finish_task();
+            });
+        } else {
+          $("#submit")
+            .off()
+            .on("click", function () {
+              $("#warning").html(warning_text);
+            });
+        }
+      });
+
+      // bind submit button
+      $("#submit").on("click", () => {
+        $("#warning").html(warning_text);
+      });
+    },
+    finish_task: function () {
+      _finish_task();
+    },
+  };
+});
