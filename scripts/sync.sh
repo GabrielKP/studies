bc26354cff25e6b0c9fb3acbcf2f7f3e69bbe6ca
Gabriel Kressin Palacios <gkressi1@jhu.edu>
2024-02-11 03:08:03 -0500

    reconfigured data structures

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,20 +1,7 @@
 #!/bin/sh
 # Author: Gabriel Kressin
 
-if [[ $1 ]]
-then
-    WHOST=$1;
-else
-    if [[ $LV_HOST ]];
-    then
-        WHOST=$LV_HOST;
-    else
-        echo "LV_HOST not set, require as first argument or set in shell";
-        exit 1
-    fi
-fi
-
 rsync -rv\
     --delete\
-    www/ $WHOST:~/psyserver/data/studies/study-fa
+    www/ $1:~/psyserver/data/studies/study-fa
 
