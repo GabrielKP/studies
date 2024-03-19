857935544ee5c576611fa59c4c83eb7643acd9fd
Gabriel Kressin Palacios <gkressi1@jhu.edu>
2024-03-18 23:03:15 -0400

    version bump to 1.0.0

--- www/linger-interference-pause/static/js/main.js
+++ www/linger-interference-pause/static/js/main.js
@@ -36,7 +36,7 @@ require([
   Complete
 ) {
   // configuration
-  let _version = "1.0.0-5";
+  let _version = "1.0.0";
   let config = {
     study: "linger-interference-pause",
     version: _version,
