fdb385cd661449e0233057e72424add954ee4212
Gabriel Kressin Palacios <gkressi1@jhu.edu>
2024-04-10 14:10:32 -0400

    theory of mind condition

---
+++ www/linger-interference-tom/static/js/component/TomQuestions.js
@@ -0,0 +1,21 @@
+define([], function () {
+  return [
+    "When Sarah gets ready, does she assume her shoes are under her dress?", // 0
+    "When the hikers arrive, do they find their cabin unoccupied?", // 1
+    "Does Ken's shoe store bag contain sandals?", // 2
+    "In reality, is Jeff's shirt pink?", // 3
+    "When Larry writes his paper, does he think the debate has been solved?", // 4
+    "Does Jill think her drink will taste like a mocha?", // 5
+    "Does Anne think the blue dish contains spaghetti?", // 6
+    "When the Garcia family arrives home, do they believe the score is 5-3?", // 7
+    "Does George think Amy is sick today?", // 8
+    "By the time Mary comes in, does John know where his keys are?", // 9
+    "Do the campers find their cooler empty of soda?", // 10
+    "Does Lisa now believe that Jacob is asleep?", // 11
+    "Does Jenny expect to find her chocolate in the cupboard?", // 12
+    "When the CEO comes to work, do they discover that all of the windows are cleaned?", // 13
+    "In reality, did Shannon receive an A on the exam?", // 14
+    "When they get up, do the girls believe the ice cream is melted?", // 15
+    "At the end of the fishing line, does George see a fish?", // 16
+  ];
+});
---
+++ www/linger-interference-tom/static/js/main.js
@@ -0,0 +1,118 @@
+require([
+  "module/Study",
+  "stage/Welcome",
+  "stage/ContentWarning",
+  "stage/Consent",
+  "stage/Fullscreen",
+  "stage/GeneralInstructions",
+  "stage/InterferenceTomTraining",
+  "stage/FreeAssociationPre",
+  "stage/Reading",
+  "stage/InterferenceTomTesting",
+  "stage/FreeAssociationPost",
+  "stage/QuestionnaireTransportation",
+  "stage/QuestionnaireComprehension",
+  "stage/QuestionnaireExperience",
+  "stage/QuestionnaireDemographics",
+  "stage/QuestionnaireOpen",
+  "stage/Complete",
+], function (
+  Study,
+  Welcome,
+  ContentWarning,
+  Consent,
+  Fullscreen,
+  GeneralInstructions,
+  InterferenceTomTraining,
+  FreeAssociationPre,
+  Reading,
+  InterferenceTomTesting,
+  FreeAssociationPost,
+  QuestionnaireTransportation,
+  QuestionnaireComprehension,
+  QuestionnaireExperience,
+  QuestionnaireDemographics,
+  QuestionnaireOpen,
+  Complete
+) {
+  // configuration
+  let _version = "2.0.0";
+  let config = {
+    study: "linger-interference-tom",
+    version: _version,
+    debug: false,
+    default_button_timeout: 500,
+    time_limit_pre: 180000,
+    time_limit_post: 180000,
+    code_completion: "CGWO2HA6",
+    code_noconsent: "CEH4RWLC",
+    code_content_warning_disagree: "CAYTAWD0",
+    studytime: 36,
+    enforce_fullscreen: true,
+    reading_delay_key: 100,
+    interference_tom_training_passage_indices: [4, 5, 6, 8, 12, 15],
+    interference_tom_testing_passage_indices: [0, 7],
+    interference_tom_time_passage: 18000,
+    interference_tom_time_question: 8000,
+    interference_tom_pause: 4000,
+    conditions: ["passage1", "passage2"],
+  };
+
+  // determine debug mode
+  // The names refer to tom, but should be treated as situation.
+  let url_params = new URLSearchParams(window.location.search);
+  if (url_params.get("mode") == "debug") {
+    config["debug"] = true;
+    config["default_button_timeout"] = 0;
+    config["time_limit_pre"] = 6000;
+    config["time_limit_post"] = 6000;
+    config["enforce_fullscreen"] = false;
+    config["interference_tom_time_passage"] = 3000;
+    config["interference_tom_time_question"] = 3000;
+    config["interference_tom_time_pause"] = 1000;
+    config["interference_tom_min_training_sessions"] = 3;
+  }
+
+  // local mode: needed to determine how data is saved
+  config["local"] = false;
+  if (url_params.has("local")) {
+    config["local"] = true;
+  }
+
+  // skip button for debug
+  if (config["debug"]) {
+    $("#container-skip-button").show();
+    $("#skip").on("click", () => {
+      if (Study.current_stage.finish_task != null) {
+        Study.current_stage.finish_task(true);
+      } else {
+        Study.next();
+      }
+    });
+  }
+
+  let initialization = Study.init(
+    [
+      Welcome,
+      ContentWarning,
+      Consent,
+      Fullscreen,
+      GeneralInstructions,
+      InterferenceTomTraining,
+      FreeAssociationPre,
+      Reading,
+      InterferenceTomTesting,
+      FreeAssociationPost,
+      QuestionnaireTransportation,
+      QuestionnaireComprehension,
+      QuestionnaireExperience,
+      QuestionnaireDemographics,
+      QuestionnaireOpen,
+      Complete,
+    ],
+    config
+  );
+  initialization.then(() => {
+    Study.next();
+  });
+});
