67c2eb4e7bd50eb95eb2397d2151b7fe8f768c59
Gabriel Kressin Palacios <gkressi1@jhu.edu>
2024-05-08 14:24:34 -0400

    initial commit geometry condition

---
+++ www/linger-interference-geometry/static/js/main.js
@@ -0,0 +1,118 @@
+require([
+  "module/Study",
+  "stage/Welcome",
+  "stage/ContentWarning",
+  "stage/Consent",
+  "stage/Fullscreen",
+  "stage/GeneralInstructions",
+  "stage/InterferenceGeometryTraining",
+  "stage/FreeAssociationPre",
+  "stage/Reading",
+  "stage/InterferenceGeometryTesting",
+  "stage/FreeAssociationPost",
+  "stage/QuestionnaireTransportation",
+  "stage/QuestionnaireComprehension",
+  "stage/QuestionnaireExperience",
+  "stage/QuestionnaireDemographics",
+  "stage/QuestionnaireOpen",
+  "stage/Complete",
+], function (
+  Study,
+  Welcome,
+  ContentWarning,
+  Consent,
+  Fullscreen,
+  GeneralInstructions,
+  InterferenceGeometryTraining,
+  FreeAssociationPre,
+  Reading,
+  InterferenceGeometryTesting,
+  FreeAssociationPost,
+  QuestionnaireTransportation,
+  QuestionnaireComprehension,
+  QuestionnaireExperience,
+  QuestionnaireDemographics,
+  QuestionnaireOpen,
+  Complete
+) {
+  // configuration
+  let _version = "1.0.0-dev1";
+  let config = {
+    study: "linger-interference-geometry",
+    version: _version,
+    debug: false,
+    default_button_timeout: 500,
+    time_limit_pre: 180000,
+    time_limit_post: 180000,
+    code_completion: "CGWO2HA6",
+    code_noconsent: "CEH4RWLC",
+    code_content_warning_disagree: "CAYTAWD0",
+    studytime: 36,
+    enforce_fullscreen: true,
+    reading_delay_key: 100,
+    interference_geometry_min_training_sessions: 3,
+    interference_geometry_training_image_indices: [1, 2, 3, 4, 5, 6],
+    interference_geometry_testing_image_index: 8,
+    interference_geometry_time_image: 18000,
+    interference_geometry_time_question: 8000,
+    interference_geometry_pause: 4000,
+  };
+
+  // determine debug mode
+  // The names refer to geometry, but should be treated as situation.
+  let url_params = new URLSearchParams(window.location.search);
+  if (url_params.get("mode") == "debug") {
+    config["debug"] = true;
+    config["default_button_timeout"] = 0;
+    config["time_limit_pre"] = 6000;
+    config["time_limit_post"] = 6000;
+    config["enforce_fullscreen"] = false;
+    config["interference_geometry_time_image"] = 6000;
+    config["interference_geometry_time_question2"] = 6000;
+    config["interference_geometry_time_pause"] = 1000;
+    config["interference_geometry_min_training_sessions"] = 3;
+  }
+
+  // local mode: needed to determine how data is saved
+  config["local"] = false;
+  if (url_params.has("local")) {
+    config["local"] = true;
+  }
+
+  // skip button for debug
+  if (config["debug"]) {
+    $("#container-skip-button").show();
+    $("#skip").on("click", () => {
+      if (Study.current_stage.finish_task != null) {
+        Study.current_stage.finish_task();
+      } else {
+        Study.next();
+      }
+    });
+  }
+
+  let initialization = Study.init(
+    [
+      Welcome,
+      ContentWarning,
+      Consent,
+      Fullscreen,
+      GeneralInstructions,
+      InterferenceGeometryTraining,
+      FreeAssociationPre,
+      Reading,
+      InterferenceGeometryTesting,
+      FreeAssociationPost,
+      QuestionnaireTransportation,
+      QuestionnaireComprehension,
+      QuestionnaireExperience,
+      QuestionnaireDemographics,
+      QuestionnaireOpen,
+      Complete,
+    ],
+    config
+  );
+  initialization.then(() => {
+    Study.next();
+  });
+});
---
+++ www/linger-interference-geometry/static/js/stage/InterferenceGeometryTraining.js
@@ -0,0 +1,125 @@
+define(["component/Pages", "component/InterferenceGeometry"], function (
+  Pages,
+  InterferencePause
+) {
+  let study;
+  let instruct_pages;
+  let task_pages;
+  let task;
+  let iteration;
+  let max_iterations;
+  let mode;
+
+  function _init_start_task() {
+    mode = "task";
+    task_pages.reset();
+    task = new InterferencePause();
+    task.init(
+      study,
+      task_pages,
+      (answered_question) => {
+        _conditional_next(answered_question);
+      },
+      study.config["interference_geometry_training_image_indices"],
+      study.config["interference_geometry_time_image"],
+      study.config["interference_geometry_time_question"],
+      study.config["interference_geometry_time_pause"],
+      iteration
+    );
+    task.start_task();
+  }
+
+  function _conditional_next(answered) {
+    iteration += 1;
+    if (iteration >= max_iterations) {
+      // TODO: study failed.
+      final_pages.reset();
+      final_pages.next();
+    } else if (
+      iteration < study.config["interference_geometry_min_training_sessions"] ||
+      !answered
+    ) {
+      if (!answered) {
+        try_again_failed.reset();
+        try_again_failed.next();
+      } else {
+        try_again_neutral.reset();
+        try_again_neutral.next();
+      }
+    } else {
+      mode = "finish";
+      final_pages.reset();
+      final_pages.next();
+      // option to practice again
+      $("#practice-again").on("click", () => {
+        _init_start_task();
+      });
+      $("#practice-again").show();
+    }
+  }
+
+  return {
+    name: "interference_geometry_training",
+    init: function (_study) {
+      study = _study;
+      instruct_pages = new Pages();
+      task_pages = new Pages();
+      try_again_neutral = new Pages();
+      try_again_failed = new Pages();
+      final_pages = new Pages();
+      iteration = 0;
+      max_iterations =
+        study.config["interference_geometry_training_image_indices"].length;
+      mode = "init";
+      return Promise.all([
+        instruct_pages.init(
+          study,
+          [
+            "interference_geometry_training/instruct-1.html",
+            "interference_geometry_training/instruct-2.html",
+            "interference_geometry_training/instruct-3.html",
+            "interference_geometry_training/instruct-4.html",
+          ],
+          () => {
+            _init_start_task();
+          }
+        ),
+        task_pages.init(study, "interference_geometry_training/task.html"),
+        try_again_failed.init(
+          study,
+          "interference_geometry_training/instruct-try_again_failed.html",
+          () => {
+            _init_start_task();
+          }
+        ),
+        try_again_neutral.init(
+          study,
+          "interference_geometry_training/instruct-try_again_neutral.html",
+          () => {
+            _init_start_task();
+          }
+        ),
+        final_pages.init(
+          study,
+          "interference_geometry_training/instruct-final.html",
+          function () {
+            study.next();
+          }
+        ),
+      ]);
+    },
+    show: function () {
+      // show instructions first
+      mode = "instructions";
+      instruct_pages.next();
+    },
+    finish_task: function (skip = false) {
+      if (mode == "instructions") {
+        _init_start_task();
+      } else {
+        task.finish_task(skip);
+        study.next();
+      }
+    },
+  };
+});
