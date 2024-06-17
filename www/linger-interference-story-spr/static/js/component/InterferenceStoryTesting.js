f0aa6a3916b2c228fd2d416adcb1d164bb65dbb5
Gabriel Kressin Palacios <gkressi1@jhu.edu>
2024-06-17 14:06:09 -0400

    initial commit story spr condition

---
+++ www/linger-interference-story-spr/static/js/component/InterferenceStoryTesting.js
@@ -0,0 +1,70 @@
+define(function () {
+  return (story = {
+    row: [
+      {
+        Story:
+          "At the nursing home, the soft and brittle were flipped twice a day to keep their skin from melding to the bedsheets.",
+        order: "1",
+        num_char: "116",
+        num_syl: "26",
+      },
+      {
+        Story:
+          "As I passed one of the cots, a papery hand grabbed mine and pressed something sticky into it.",
+        order: "2",
+        num_char: "93",
+        num_syl: "23",
+      },
+      {
+        Story: "It’s candy, the old woman said.",
+        order: "3",
+        num_char: "31",
+        num_syl: "9",
+      },
+      {
+        Story: "I opened my hand to look.",
+        order: "4",
+        num_char: "25",
+        num_syl: "7",
+      },
+      {
+        Story: "Some were oozing from their wrappers, some had teeth marks.",
+        order: "5",
+        num_char: "59",
+        num_syl: "12",
+      },
+      {
+        Story: "Some were whole and new.",
+        order: "6",
+        num_char: "24",
+        num_syl: "5",
+      },
+      {
+        Story:
+          "They were from a brand that had gone out of business in my childhood.",
+        order: "7",
+        num_char: "69",
+        num_syl: "16",
+      },
+      {
+        Story: "“It’s dementia”, a passing nurse explained.",
+        order: "8",
+        num_char: "43",
+        num_syl: "12",
+      },
+      {
+        Story: "“No, it’s candy”, the old woman said.",
+        order: "9",
+        num_char: "37",
+        num_syl: "10",
+      },
+      {
+        Story:
+          "“No”, the nurse said, carrying a bucket of human waste out of the room, “it’s dementia”.",
+        order: "10",
+        num_char: "88",
+        num_syl: "24",
+      },
+    ],
+  });
+});
