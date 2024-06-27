b7d5648f24d827c5922fb4197903b90ac4189931
Gabriel Kressin Palacios <gkressi1@jhu.edu>
2024-06-27 14:18:22 -0400

    linger-fa-dark-bedroom initial version

---
+++ www/linger-fa-dark-bedroom/comments.md
@@ -0,0 +1,7 @@
+# Comments
+
+## Decision process interference story v2
+
+Carver: 2,347 words 11,713 characters (without \*\*\*)
+
+90th quantile is ~14.5 min
---
+++ www/linger-fa-dark-bedroom/static/js/component/DarkBedroom.js
@@ -0,0 +1,107 @@
+define(function () {
+  return (story = {
+    row: [
+      {
+        Story: "***",
+        order: "1",
+        num_char: "3",
+        num_syl: "1",
+      },
+      {
+        Story:
+          "Six years ago the bulb in our overhead fan burned out and got really dim.",
+        order: "2",
+        num_char: "73",
+        num_syl: "20",
+      },
+      {
+        Story: "I bought a replacement bulb.",
+        order: "3",
+        num_char: "28",
+        num_syl: "7",
+      },
+      {
+        Story: "Same problem.",
+        order: "4",
+        num_char: "13",
+        num_syl: "3",
+      },
+      {
+        Story:
+          "I realized the fan unit was bad but didn’t really want to replace it.",
+        order: "5",
+        num_char: "69",
+        num_syl: "19",
+      },
+      {
+        Story: "Since then, my wife and I have had to live in darkness.",
+        order: "6",
+        num_char: "55",
+        num_syl: "13",
+      },
+      {
+        Story: "At night we use bedside lamps and bathroom lights.",
+        order: "7",
+        num_char: "50",
+        num_syl: "11",
+      },
+      {
+        Story:
+          "I also keep some flashlights in the bedroom for when we are cleaning or looking for something etc.",
+        order: "8",
+        num_char: "98",
+        num_syl: "27",
+      },
+      {
+        Story: "***",
+        order: "9",
+        num_char: "3",
+        num_syl: "1",
+      },
+      {
+        Story: "This winter I decided I would replace the damn thing.",
+        order: "10",
+        num_char: "53",
+        num_syl: "14",
+      },
+      {
+        Story: "But before, I tried one last time with another bulb.",
+        order: "11",
+        num_char: "52",
+        num_syl: "13",
+      },
+      {
+        Story: "So I'm up there installing the new bulb.",
+        order: "12",
+        num_char: "40",
+        num_syl: "11",
+      },
+      {
+        Story: "I grab the remote (the fan has a remote).",
+        order: "13",
+        num_char: "41",
+        num_syl: "11",
+      },
+      {
+        Story:
+          "And as I'm turning the light on I realize: the goddamn thing is dimmable.",
+        order: "14",
+        num_char: "73",
+        num_syl: "20",
+      },
+      {
+        Story:
+          "For six fucking years we lived with the inconvenience of no overhead light and the whole time it was just because the damn thing was set to “dim”.",
+        order: "15",
+        num_char: "146",
+        num_syl: "35",
+      },
+      {
+        Story: "***",
+        order: "16",
+        num_char: "3",
+        num_syl: "1",
+      },
+    ],
+  });
+});
