a06d9c696cca99d93f7d5d625361a2ccd500d15f
Gabriel Kressin Palacios <gkressi1@jhu.edu>
2025-02-10 11:36:38 -0500

    v1.0.0

--- www/linger-interference-story-spr-end/static/js/main.js
+++ www/linger-interference-story-spr-end/static/js/main.js
@@ -52,7 +52,7 @@ require([
   Complete
 ) {
   // configuration
-  let _version = "1.0.0-dev9";
+  let _version = "1.0.0";
   let config = {
     study: "linger-interference-story-spr-end",
     version: _version,
