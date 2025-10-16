d270828a721389bb8e37e1f6b1b2c9c572e2a84c
Gabriel Kressin Palacios <gkressi1@jhu.edu>
2025-10-16 15:41:52 -0400

    bugfixes, instruction changes

--- www/linger-neutralcue2-intentional/static/js/main.js
+++ www/linger-neutralcue2-intentional/static/js/main.js
@@ -36,7 +36,7 @@ require([
   Complete
 ) {
   // configuration
-  let _version = "1.0.0";
+  let _version = "1.0.1-alpha1";
   let config = {
     study: "linger-neutralcue2-intentional",
     version: _version,
